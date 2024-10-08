aebbe062cab2f565595120d946ff536d96931700
Maxime <maxime.lecourt457@gmail.com>
2024-10-08 10:32:44 +0200

    Add navbar

---
+++ src/views/presentation.js
@@ -0,0 +1,24 @@
+const viewPresentation = () => `<section>
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+        <img
+          alt=""
+          src="./assets/images/Moi.png"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-24">
+        <h2 class="text-3xl font-bold sm:text-4xl">Formation</h2>
+
+        <p class="mt-4 text-gray-600">
+        BACHELOR,DEVELOPPEUR CONCEPTEUR  D’APPLICATION de sept. 2023 à août 2026 CODA, Orléans
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+`;
+
+export default viewPresentation;
